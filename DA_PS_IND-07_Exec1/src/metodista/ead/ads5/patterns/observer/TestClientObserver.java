ab638bec05e267d23df8c7dc35ff0b92bc673144
LuisGustavo <LuisGustavo@luis-gustavo-pc>
2016-09-22 22:16:15 -0300

    - Incluído classes de test para Observer e Strategy;

---
+++ DA_PS_IND-07_Exec1/src/metodista/ead/ads5/patterns/observer/TestClientObserver.java
@@ -0,0 +1,13 @@
+package metodista.ead.ads5.patterns.observer;
+
+/**
+ *
+ * @author LuisGustavo
+ */
+public class TestClientObserver {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ DA_PS_IND-07_Exec1/src/metodista/ead/ads5/patterns/strategy/TestClientStrategy.java
@@ -0,0 +1,13 @@
+package metodista.ead.ads5.patterns.strategy;
+
+/**
+ *
+ * @author LuisGustavo
+ */
+public class TestClientStrategy {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
