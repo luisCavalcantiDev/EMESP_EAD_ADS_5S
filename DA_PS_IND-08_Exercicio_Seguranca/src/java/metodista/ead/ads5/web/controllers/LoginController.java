ed5eeed50bcd2a8d9f69211a0445192829d39158
LuisGustavo <LuisGustavo@luis-gustavo-pc>
2016-10-02 10:16:45 -0300

    - Incluído projeto web JSP para exerício; - Ajustes nos projetos JSP para exercícios;

--- DA_PS_IND-08_Exercicio_Seguranca/src/java/metodista/ead/ads5/web/controllers/LoginController.java
+++ DA_PS_IND-08_Exercicio_Seguranca/src/java/metodista/ead/ads5/web/controllers/LoginController.java
@@ -25,8 +25,31 @@ public class LoginController extends HttpServlet {
         try {
 
             String user = request.getParameter("user");
-            String pass = request.getParameter("pass");
+            String pass = request.getParameter("pass");                        
+            
+            PrintWriter output = response.getWriter();
+            output.println("<html>");
+            output.println("<head>");
+            output.println("</head>");
+            output.println("<body>");
+            output.println("<div>Login efetuado com sucesso!!! (teste)</div>");
+            output.println("<div>Usuário: " + user + "</div>");
+            output.println("<div>Senha: " + pass + "</div>");
+            output.println("</body>");
+            output.println("</html>");
 
+        } catch (Exception e) {
+            throw new ServletException("Erro ao processar requisição: " + e.getMessage());
+        }
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        try {
+
+            String user = request.getParameter("user");
+            String pass = request.getParameter("pass");                        
+            
             PrintWriter output = response.getWriter();
             output.println("<html>");
             output.println("<head>");
