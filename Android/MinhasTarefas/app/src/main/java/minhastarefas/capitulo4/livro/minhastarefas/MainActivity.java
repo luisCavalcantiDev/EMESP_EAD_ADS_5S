ad4a6f08939ec26352b78f3c63f217e0bd6b861b
luis.pereira <luis.pereira@LUISP.GRUPOSKILL205.LOCAL>
2016-10-05 13:09:29 -0300

    - Adicionado projeto apk capitulo 4 do livro; - Criação tela inicial de login;

---
+++ Android/MinhasTarefas/app/src/main/java/minhastarefas/capitulo4/livro/minhastarefas/MainActivity.java
@@ -0,0 +1,13 @@
+package minhastarefas.capitulo4.livro.minhastarefas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
